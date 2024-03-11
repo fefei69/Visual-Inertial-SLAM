32bd973482c6c1a8636a1166d97f87333412932e
fefei69 <coolpixs04@gmail.com>
2024-03-10 23:43:28 -0700

    small update

--- code/main.py
+++ code/main.py
@@ -7,7 +7,13 @@ if __name__ == '__main__':
 	# Load the measurements
 	filename = "../data/10.npz"
 	t,features,linear_velocity,angular_velocity,K,b,imu_T_cam = load_data(filename)
-
+	print("t: ", t)
+	print("features: ", features)
+	print("linear_velocity: ", linear_velocity)
+	print("angular_velocity: ", angular_velocity)
+	print("K: ", K)
+	print("b: ", b)
+	print("imu_T_cam: ", imu_T_cam)
 	# (a) IMU Localization via EKF Prediction
 
 	# (b) Landmark Mapping via EKF Update
